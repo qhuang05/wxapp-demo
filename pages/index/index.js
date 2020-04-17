c19d29500eb280e317b2d9e3f05cd6844f61a2ee
qhuang05 <qhuang05@sina.com>
2020-04-17 17:46:40 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,154 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    data: {
+        background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 2000,
+        duration: 500,
+    },
+    onLoad() {
+        // wx.showToast({
+        //     title: '成功',
+        //     icon: 'success',
+        //     duration: 2000
+        // })
+        // wx.showModal({
+        //     title: '提示',
+        //     content: '这是一个模态弹窗',
+        //     success(res) {
+        //         if (res.confirm) {
+        //             console.log('用户点击确定')
+        //         } else if (res.cancel) {
+        //             console.log('用户点击取消')
+        //         }
+        //     }
+        // })
+        // wx.showLoading({
+        //     title: '加载中',
+        // })
+        // wx.showActionSheet({
+        //     itemList: ['A', 'B', 'C'],
+        //     success(res) {
+        //         console.log(res.tapIndex)
+        //     },
+        //     fail(res) {
+        //         console.log(res.errMsg)
+        //     }
+        // })
+        // wx.showNavigationBarLoading()
+        // wx.setNavigationBarTitle({
+        //     title: '当前页面'
+        // })
+        // wx.setNavigationBarColor({
+        //     frontColor: '#ffffff',
+        //     backgroundColor: '#ff0000',
+        //     animation: {
+        //         duration: 400,
+        //         timingFunc: 'easeIn'
+        //     }
+        // })
+        // wx.setTopBarText({
+        //     text: 'hello, world!'
+        // })
+
+        // wx.getLocation({
+        //     type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+        //     success(res) {
+        //         const latitude = res.latitude
+        //         const longitude = res.longitude
+        //         wx.openLocation({
+        //             latitude,
+        //             longitude,
+        //             scale: 18
+        //         })
+        //     }
+        // })
+
+        // wx.showShareMenu({
+        //     withShareTicket: false
+        // })
+    },
+    onShow() {},
+    showEnv() {
+        // wx.switchTab({
+        //     url: '/pages/cart/cart',
+        //     success: function(){
+        //     },
+        //     fail: function(){
+        //     }
+        // })
+    },
+    clickHandler(){
+        // wx.chooseAddress({
+        //     success(res) {
+        //         wx.showToast({
+        //             title: res.telNumber,
+        //             icon: 'success',
+        //             duration: 2000
+        //         })
+        //         console.log(res.userName)
+        //         console.log(res.postalCode)
+        //         console.log(res.provinceName)
+        //         console.log(res.cityName)
+        //         console.log(res.countyName)
+        //         console.log(res.detailInfo)
+        //         console.log(res.nationalCode)
+        //         console.log(res.telNumber)
+        //     }
+        // });
+
+        wx.scanCode({
+            success(res) {
+                wx.showModal({
+                    title: '提示',
+                    content: JSON.stringify(res),
+                    success(res) {
+                        if (res.confirm) {
+                            console.log('用户点击确定')
+                        } else if (res.cancel) {
+                            console.log('用户点击取消')
+                        }
+                    }
+                })
+            }
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    },
+    gotoTop(){
+        wx.pageScrollTo({
+            selector: '#tt2'
+        })
+    },
+    fileUpload(){
+        wx.chooseImage({
+            sourceType: ['album'],
+            success(res) {
+                const tempFilePaths = res.tempFilePaths;
+                console.log(tempFilePaths);
+                wx.uploadFile({
+                    url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+                    filePath: tempFilePaths[0],
+                    name: 'file',
+                    formData: {
+                        'user': 'test'
+                    },
+                    success(res) {
+                        const data = res.data
+                        //do something
+                        wx.showModal({
+                            title: '提示',
+                            content: JOSN.stringify(data),
+                            success(res) {
+                                if (res.confirm) {
+                                    console.log('用户点击确定')
+                                } else if (res.cancel) {
+                                    console.log('用户点击取消')
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+        })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
