3d80f9524efc376a70ed2ab42f8840d33d8024ce
qhuang05 <qhuang05@sina.com>
2020-05-07 18:19:51 +0800

    update

--- pages/movies_detail/detail.js
+++ pages/movies_detail/detail.js
@@ -1,11 +1,17 @@
-// pages/movies_detail/detail.js
+const movieInfo = {
+    src: "http://vali.cp31.ott.cibntv.net/youku/67733D1453741719DBF414C53/03000801005AC32A877B6C003E8803898A50A7-4AA2-D56A-8779-C0AC02483896.mp4?sid=052523731095219522262_00_Adb554c5b9330dcfc17e172878984aac2&amp;sign=35860c09f57a1703763a7d20a6226b08&amp;ctype=50&amp;hd=1",
+    "title": '哈里波特',
+    "rate": '8.6',
+    "type": '科幻,悬疑/2016',
+    "desc": "从小寄养在姨丈家里的哈利波特，饱受姨父一家人的歧视与欺侮，然而就在11岁生日那天，哈利波特得知了自己的身世，他的生活也随之发生了天翻地覆的改变。原来，哈利的父母是两位巫师，在同黑巫师的较量中被杀害了。\n......"
+}
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        movie: movieInfo
     },
 
     /**
